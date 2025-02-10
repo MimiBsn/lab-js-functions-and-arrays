0c7bd5136a2f6fb639da52fe6faad42b56aafe43
Alexandra Busson <159456749+MimiBsn@users.noreply.github.com>
2025-02-10 14:13:36 +0100

    solved

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,17 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+    let maxNum = 0;
+
+    if(num1 > num2){
+        maxNum = num1
+    } else if( num2 > num1 ){
+        maxNum = num2
+    } else if(num1 == num2){
+        maxNum = num1
+    }
+
+    return maxNum;
+}
 
 
 
@@ -7,7 +19,21 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    let longWord = "";
+
+    words.forEach(e => {
+        if(e.length > longWord.length){
+            longWord = e
+        }
+    });
+
+    if(words.length == 0){
+        return null
+    } else{
+        return longWord;
+    }
+}
 
 
 
@@ -15,15 +41,45 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    
+    let num = 0;
 
+    numbers.forEach(e => {
+        num += e;
+    })
 
+    if(numbers.length == 0){
+        return 0;
+    }else{
+        return num;
+    }
+
+    // return num
+}
+
+    
 
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+    let sum = 0;
+
+    numbers2.forEach(e => {
+        sum += e;
+    });
+
+    const total = sum / numbers2.length;
+    console.log(total)
+    if(numbers2.length == 0){
+        return sum;
+    }else{
+        return total;
+    }
+
+}
 
 
 
@@ -31,4 +87,13 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words2, wordToSearch) {
+
+    if(words2.find((words2) => words2 === wordToSearch)){
+        return true
+    }else if(words2.length == 0){
+        return null;
+    } else{
+        return false
+    }
+}
